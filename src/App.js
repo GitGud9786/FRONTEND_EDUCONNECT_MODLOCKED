22b05591eddae3909d09e47afc29fb7b59935720
GitGud9786 <tausif1250@gmail.com>
2024-10-10 02:16:02 +0600

    Merged the jsx files and css files into one singular jsx file and one css file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import Sidebar from './components/sidebar/sidebar';
-import Courseinfo from './components/courseinfo/courseinfo';
-import ToDoList from './components/todolist/todolist';
-import PageHeader from './components/pageheader/pageheader';
+import CombinedComponent from './components/CoursePage';
 
 const App = () => {
   return (
     <div className="App">
-      <PageHeader />
-      <div className="horizontalcontainer"> {/* Added this container */}
-        <Sidebar />
-        <Courseinfo />
-        <ToDoList />
-      </div>
+      <CombinedComponent />;
     </div>
   );
 }
