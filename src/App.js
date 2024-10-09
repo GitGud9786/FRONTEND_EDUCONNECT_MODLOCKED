47a0fe9fb317331086cb650a2810360abfd3d21f
Shahir-Awlad <shahir.thebomb@gmail.com>
2024-10-10 00:55:53 +0600

    Added 2 pages (Login and Dashboard) and routed them together

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+
+import Login from "./Components/Login";
+import Dashboard from "./Components/Dashboard";
+
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+            <Routes>
+            <Route path="/" element={<Login/>} />
+            <Route path="/dash" element={<Dashboard/>} />
+
+            </Routes>
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
