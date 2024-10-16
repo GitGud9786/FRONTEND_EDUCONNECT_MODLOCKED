b454074652c75a69c26215d0c94e83171cae902e
Shahir-Awlad <shahir.thebomb@gmail.com>
2024-10-16 19:48:38 +0600

    Added more changes to the home page

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -3,7 +3,7 @@ import '../styles/Dashboard.css'
 import CourseCard from './CourseCard';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faCalendar, faBook, faUsers, faEnvelope, faChartBar, faCog, faAddressBook, faSearch, faMessage } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faCalendar, faBook, faChartBar, faCog, faMessage, faTh, faList } from '@fortawesome/free-solid-svg-icons';
 import logo from '../Assets/logo.jpg';
 
 const Dashboard = () => {
@@ -90,8 +90,27 @@ const Dashboard = () => {
               <span className="date-home">Shahir Awlad</span>
             </li>
           </ul>
+        </div>        
+      </div>
+
+      <div className="main-home">
+        <div className="main-left-home">
+          <div className="maintop-home">
+            <h1>Home</h1>
+
+            <div className="view-home">
+              <button className="nav-home-selected">
+                <FontAwesomeIcon icon={faTh} className="icon-home" />
+                <span>GridView</span>
+              </button>
+              <button className="nav-home">
+                <FontAwesomeIcon icon={faList} className="icon-home" />
+                <span>ListView</span>
+              </button>
+            </div>
+            
+          </div>
         </div>
-        
       </div>
     </div>
   )
