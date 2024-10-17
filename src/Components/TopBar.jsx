a138397f75b319b15e8176d71ef08557111d2078
Shahir-Awlad <shahir.thebomb@gmail.com>
2024-10-17 11:31:48 +0600

    Did a lot

---
+++ src/Components/TopBar.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import '../styles/Dashboard.css'
+import { Link, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faCalendar, faBook, faChartBar, faCog, faTh, faList, faMessage } from '@fortawesome/free-solid-svg-icons';
+import logo from '../Assets/logo.jpg';
+
+
+const TopBar = () => {
+    const currentDate = new Date();
+    const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+
+  return (
+    <div>
+      <div className="topbar-home">
+        <div className="topbar-left-home">
+        <img src={logo} alt="" className="logo-home" />
+        <ul>
+          <li >
+          <button className="nav-home-selected">
+            <FontAwesomeIcon icon={faHome} className="icon-home" />
+            <span>Home</span>
+          </button>
+
+          <Link to = "/course" className="nav-home">
+            <FontAwesomeIcon icon={faBook} className="icon-home" />
+            <span>Courses</span>
+          </Link>
+          
+          <button className="nav-home">
+            <FontAwesomeIcon icon={faCalendar} className="icon-home" />
+            <span>Schedule</span>
+          </button>
+
+          <button className="nav-home">
+            <FontAwesomeIcon icon={faMessage} className="icon-home" />
+            <span>Messages</span>
+          </button>
+
+          <button className="nav-home">
+            <FontAwesomeIcon icon={faChartBar} className="icon-home" />
+            <span>Grades</span>
+          </button>
+          
+          </li>
+        </ul>
+        </div>
+
+        <div className="topbar-right-home">
+          <ul>
+            <li>
+              <button className="nav-home">
+              <FontAwesomeIcon icon={faCog} className="icon-home" />
+              </button>
+              <span className="date-home">{formattedDate}</span>
+              <span className="date-home">Shahir Awlad</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TopBar
