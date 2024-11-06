3a165a8f12889d869390ff8c945df14cdd55a4a9
GitGud9786 <tausif1250@gmail.com>
2024-11-06 19:46:15 +0600

    Added admin registration panels for student and faculty

---
+++ src/Components/AdminFacultyRegister.jsx
@@ -0,0 +1,157 @@
+import React, { useState } from 'react';
+import '../styles/AdminFacultyRegister.css';
+
+const AdminFacultyRegister = () => {
+  const [formData, setFormData] = useState({
+    facultyfirstName: '',
+    facultymiddleName: '',
+    facultylastName: '',
+    facultydateOfBirth: '',
+    facultyId: '',
+    facultyDesignation: '',
+    facultyDepartment: '',
+    facultystreetAddress: '',
+    facultycity: '',
+    facultystate: '',
+    facultycountry: '',
+    facultyzipCode: '',
+    facultyemail: '',
+    facultyphone: '',
+    facultybloodGroup: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Data Submitted:", formData);
+  };
+
+  return (
+    <form className="adminfacultyform" onSubmit={handleSubmit}>
+      <h2>Faculty Register Form</h2>
+      <div className="adminfacultyformsection">
+        <input
+          type="text"
+          name="facultyfirstName"
+          placeholder="First Name"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="facultymiddleName"
+          placeholder="Middle Name"
+          value={formData.middleName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="facultylastName"
+          placeholder="Last Name"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+        <input
+          type="facultydate"
+          name="facultydateOfBirth"
+          placeholder="Date of Birth"
+          value={formData.dateOfBirth}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="facultybloodgroup"
+          placeholder="Blood Group"
+          value={formData.bloodGroup}
+          onChange={handleChange}
+        />
+      </div>
+
+      <h2>Designation and Department</h2>
+      <div className="adminfacultyformsection">
+      <input
+          type="text"
+          name="facultydesignation"
+          placeholder="Desgination of the faculty"
+          value={formData.facultyDesignation}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="facultydepartment"
+          placeholder='Designation of department'
+          value={formData.facultyDepartment}
+          onChange={handleChange}
+        />
+      </div>
+
+      <h2>Address</h2>
+      <div className="adminfacultyformsection">
+        <input
+          type="text"
+          name="facultystreetAddress"
+          placeholder="Street Address"
+          value={formData.streetAddress}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="facultycity"
+          placeholder="City"
+          value={formData.city}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="facultystate"
+          placeholder="State / Province"
+          value={formData.state}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="facultycountry"
+          placeholder="Country"
+          value={formData.country}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="facultyzipCode"
+          placeholder="ZIP Code"
+          value={formData.zipCode}
+          onChange={handleChange}
+        />
+      </div>
+
+      <h2>Contact Information</h2>
+      <div className="adminfacultyformsection">
+        <input
+          type="email"
+          name="facultyemail"
+          placeholder="E-mail"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="facultyphone"
+          placeholder="Phone"
+          value={formData.phone}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button className='facultyregisterbutton' type="submit">Register this faculty</button>
+    </form>
+  );
+};
+
+export default AdminFacultyRegister;
